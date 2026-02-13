33cc8c2e926ae6ec7c1709941d3a8cc9f84af24d
itsumura-h <dumblepy@gmail.com>
2026-02-13 03:26:14 +0000

    pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,8 @@ import preact from '@preact/preset-vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	// GitHub Pages で必要に応じてベースパスを設定
+	base: process.env.GITHUB_ACTIONS ? '/jyokyo/' : '/',
 	plugins: [
 		preact({
 			prerender: {
