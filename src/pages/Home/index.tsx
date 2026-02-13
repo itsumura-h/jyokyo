d24d990881c6ff7067d6c7713597807b9b6feae9
itsumura-h <dumblepy@gmail.com>
2026-02-13 04:55:32 +0000

    add event

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,13 @@
 import { Hero } from '../../components/Hero'
-import { Latest } from "../../components/Latest"
+import { Latest } from '../../components/Latest'
+import { Event } from '../../components/Event'
 
 export function Home() {
   return (
     <>
       <main>
         <Hero />
+        <Event />
         <Latest />
       </main>
     </>
